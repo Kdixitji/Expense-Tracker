dd8c52d94b687888c525e27bd1b178063693cf57
Kratik Sharma <98703533+Kdixitji@users.noreply.github.com>
2023-02-11 19:41:41 +0530

    MongoDB Atlas Database

--- server/Database/mongodb.js
+++ server/Database/mongodb.js
@@ -3,7 +3,6 @@ import { mongoose } from "mongoose";
 async function connect() {
     await mongoose.connect(`${process.env.MONGODB_URL}`);
     console.log("connected to mongodb");
-
 }
 
 export default connect;
